4d6e05048b2a272ba90403b0e393cbc23017b3a2
arturosabalsa <sabalsa29@gmail.com>
2020-06-06 09:03:37 -0500

    Se añadio un nuevo documento HTML

---
+++ src/indexjs.js
@@ -0,0 +1,3 @@
+function entrar(){
+    alert("Listo");
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-alert("Este es un mensaje de alerta para sabalsa");
+alert("El documento js esta enlazado")
+function entrar(){
+    
+var done=0;
+var usuario=document.usuario.value;
+var password=document.password.value;
+
+if (usuario=="sabalsa" && password=="1234") { 
+    window.location="TU_PAGINA_WEB.HTML";
+    alert("Este es un mensaje de alerta para sabalsa");
+    }
+}
